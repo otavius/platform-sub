4e9b09f7c01f495a49daef0324a3f1e60f5e3342
otavius <developer.otavius@gmail.com>
2024-12-15 13:51:50 -0500

    added models for writer app and forms

--- writer/urls.py
+++ writer/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("writer-dashboard/", views.writer_dashboard, name="writer-dashboard"),
+    path("create-article/", views.create_article, name="create-article"),
 ]
--- writer/views.py
+++ writer/views.py
@@ -1,7 +1,30 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from .forms import ArticleForm
+from django.http import HttpResponse
 # Create your views here.
+
 @login_required(login_url="my-login")
 def writer_dashboard(request):
     
     return render(request, "writer/writer-dashboard.html")
+
+@login_required(login_url="my-login")
+def create_article(request):
+    form = ArticleForm()
+
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.user = request.user
+
+            article.save()
+            return HttpResponse("Article Creadted!")
+        
+    context = {"CreateArticleForm": form}
+
+    return render(request, "writer/create-article.html", context)
+
+
